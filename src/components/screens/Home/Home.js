29d64dd09f5d8f5e0a75a1ce65f280c6f46f6943
plamen1982 <plamen.hristov@ymail.com>
2019-11-28 14:21:31 +0200

    remove unused variables

--- src/components/screens/Home/Home.js
+++ src/components/screens/Home/Home.js
@@ -1,14 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Grid } from '@material-ui/core';
 import { Container } from '@material-ui/core';
 import useCreateMap from '../../hooks/useCreateMap';
 import RadioGroup from '../../common/RadioGroup/RadioGroup';
 import Selects from '../../common/Selects/Selects';
 const DrawAMap = () => { 
-    // const [zoom, setZoom] = useState(5);
-    // const [center, setCenter] = useState([37.41, 8.82]);
+    
+    const [zoom, setZoom] = useState(5);
+    const [center, setCenter] = useState([37.41, 8.82]);
     const map = useCreateMap('map-container', zoom, center);
     console.log(map);
+    useEffect(() => { 
+        setZoom(7);
+        setCenter([37.00, 8.00]);
+     }, [zoom, center])
     return (
         <Container style={{paddingTop: 50}}>
             <Grid container spacing={5}>
